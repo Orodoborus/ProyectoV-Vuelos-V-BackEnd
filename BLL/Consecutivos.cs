0d825d36c1d1e5eaa166e8ab5bcf879db65c333b
D'andre Highfield <dwolbromh070@ulacit.ed.cr>
2021-03-16 21:38:26 -0600

    Consecutivos 100%

--- BLL/Consecutivos.cs
+++ BLL/Consecutivos.cs
@@ -12,9 +12,9 @@ namespace BLL
     public class Consecutivos
     {
         #region propfulls
-        private string _codigo_consecutivo;
+        private int _codigo_consecutivo;
 
-        public string Codigo_Consecutivo
+        public int Codigo_Consecutivo
         {
             get { return _codigo_consecutivo; }
             set { _codigo_consecutivo = value; }
@@ -60,6 +60,22 @@ namespace BLL
             set { _rango_fin = value; }
         }
 
+
+        static int consID = 0;
+
+        public static int GlobalValue
+        {
+            get
+            {
+                return consID;
+            }
+            set
+            {
+                consID = value;
+            }
+        }
+
+
         #endregion
 
         #region variables privadas
@@ -71,6 +87,59 @@ namespace BLL
         #endregion
 
         #region Methods
+
+        public List<Consecutivos> getAllCons(ref string mensaje_error, ref int numero_error)
+        {
+            connection = cls_DAL.trae_conexion("ServiciosWeb", ref mensaje_error, ref numero_error);
+            if (connection == null)
+            {
+                return null;
+            }
+            else
+            {
+                sql = "exec get_all_consecutivos";
+                ds = cls_DAL.ejecuta_dataset(connection, sql, false, ref mensaje_error, ref numero_error);
+                if (numero_error != 0)
+                {
+                    return null;
+                }
+                else
+                {
+                    return procesarConsecutivos(ds.Tables[0]);
+                }
+            }
+        }
+
+        public void createCons(ref string mensaje_error, ref int numero_error, int Codigo_Consecutivo, string Descripcion,string Valor, string prefijo, string Rango_ini, string Rango_Fin)
+        {
+            connection = cls_DAL.trae_conexion("ServiciosWeb", ref mensaje_error, ref numero_error);
+            sql = "exec create_new_consecutive @Codigo_Consecutivo = "+Codigo_Consecutivo+", @Descripcion = '"+ Descripcion +
+                "', @Valor = '"+Valor+"', @Prefijo = '"+ prefijo + "', @Rango_Ini = '"+ Rango_ini + "', @Rango_Fin = '"+ Rango_Fin + "'";
+            ds = cls_DAL.ejecuta_dataset(connection, sql, false, ref mensaje_error, ref numero_error);
+        }
+
+        public void updateCons(ref string mensaje_error, ref int numero_error, string Descripcion, string Valor, string prefijo, string Rango_ini, string Rango_fin)
+        {
+            connection = cls_DAL.trae_conexion("ServiciosWeb", ref mensaje_error, ref numero_error);
+            sql = "exec consecutive_update_cambios @Descripcion = '" + Descripcion + "', @Valor = '" + Valor + "', @Prefijo = '" + prefijo + "', @Rango_ini = '" + Rango_ini + "', @Rango_fin = '" + Rango_fin + "'";
+            ds = cls_DAL.ejecuta_dataset(connection, sql, false, ref mensaje_error, ref numero_error);
+        }
+
+        private List<Consecutivos> procesarConsecutivos(DataTable dt)
+        {
+            return (from DataRow dr in dt.Rows
+                    select new Consecutivos()
+                    {
+                        Codigo_Consecutivo = Convert.ToInt32(dr["Codigo_Consecutivo"]),
+                        Descripcion = dr["Descripcion"].ToString(),
+                        Valor = dr["Valor"].ToString(),
+                        Prefijo = dr["Prefijo"].ToString(),
+                        Rango_Ini = dr["Rango_Ini"].ToString(),
+                        Rango_Fin = dr["Rango_Fin"].ToString()
+                    }).ToList();
+        }
+
+
 
         #endregion
     }
--- ProyectoV/Controllers/ConsecutivosController.cs
+++ ProyectoV/Controllers/ConsecutivosController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using ProyectoV.Models;
 using BLL;
 using Newtonsoft.Json;
 
@@ -11,31 +12,51 @@ namespace ProyectoV.Controllers
 {
     public class ConsecutivosController : ApiController
     {
+
+        string mensaje_error;
+        int numero_error;
         // GET: api/Consecutivos
-        public IEnumerable<string> Get()
+        public List<Consecutivos> Get()
         {
-            return new string[] { "value1", "value2" };
+            List<Consecutivos> listaConsecutivos = new Consecutivos().getAllCons(ref mensaje_error, ref numero_error);
+            return listaConsecutivos;
         }
 
         // GET: api/Consecutivos/5
-        public string Get(int id)
+        public Consecutivos Get(int id)
         {
-            return "value";
+            List<Consecutivos> listaConsecutivos = new Consecutivos().getAllCons(ref mensaje_error, ref numero_error);
+            crypting c = new crypting();
+            Consecutivos x = listaConsecutivos.ElementAt(id);
+            Consecutivos spes = new Consecutivos();
+            spes.Codigo_Consecutivo = x.Codigo_Consecutivo;
+            spes.Descripcion = c.decrypt(x.Descripcion);
+            spes.Valor = c.decrypt(x.Valor);
+            spes.Prefijo = c.decrypt(x.Prefijo);
+            spes.Rango_Ini = c.decrypt(x.Rango_Ini);
+            spes.Rango_Fin = c.decrypt(x.Rango_Fin);
+            return spes;
         }
 
         // POST: api/Consecutivos
-        public void Post([FromBody]string value)
+        public void Post([FromBody]Consecutivos cons)
         {
+            Consecutivos x = new Consecutivos();
+            crypting c = new crypting();
+            x.createCons(ref mensaje_error, ref numero_error, Consecutivos.GlobalValue = Consecutivos.GlobalValue + 1, c.encrypt(cons.Descripcion),c.encrypt(cons.Valor), c.encrypt(cons.Prefijo), c.encrypt(cons.Rango_Ini), c.encrypt(cons.Rango_Fin));
         }
 
         // PUT: api/Consecutivos/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]Consecutivos cons)
         {
+            Consecutivos x = new Consecutivos();
+            crypting c = new crypting();
+            x.updateCons(ref mensaje_error, ref numero_error, c.encrypt(cons.Descripcion), c.encrypt(cons.Valor), c.encrypt(cons.Prefijo), c.encrypt(cons.Rango_Ini), c.encrypt(cons.Rango_Fin));
         }
 
         // DELETE: api/Consecutivos/5
         public void Delete(int id)
         {
-        }
+        } 
     }
 }
