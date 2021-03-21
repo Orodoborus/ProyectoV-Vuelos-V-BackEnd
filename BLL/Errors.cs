c77f838b391239c54eb8f98857168a7caa7c12df
Diego <dicg47@gmail.com>
2021-03-20 18:42:42 -0600

    Error Managament
    Cambios a la base:
    
    ALTER TABLE Errors
    ADD Date varchar(255), Error_Number varchar(255)
    
    
    CREATE PROCEDURE get_all_errors
    AS
    SELECT * FROM Errors
    GO
    
    CREATE PROCEDURE register_error
    @Error_ID varchar(255), @Error_Message varchar(MAX), @Time varchar(255), @Date varchar(255), @Error_Number varchar(255)
    as
    INSERT INTO dbo.Errors(Error_ID,Error_Message,Time,Date,Error_Number)
    VALUES
    (@Error_ID,@Error_Message,@Time,@Date,@Error_Number);
    GO
    
    
    Tambien esta en el drive
    Date y time estan separados, para facilitar los filtros.
    Deje unos comments con unas preguntas, para verlas despues

--- BLL/Errors.cs
+++ BLL/Errors.cs
@@ -7,6 +7,14 @@ using System.Text;
 using System.Threading.Tasks;
 using DAL;
 
+/*
+ 4.6.2. Errores
+Consiste en mostrar una lista con los errores que se han registrado en la aplicación. La 
+consulta tendrá la opción de filtrar por rango de fecha. Se presenta la fecha y hora, numero 
+de error y el mensaje de error correspondiente.
+
+ */
+
 namespace BLL
 {
     public class Errors
@@ -36,6 +44,22 @@ namespace BLL
             set { _time = value; }
         }
 
+        private string _date;
+
+        public string Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+        private string _error_number;
+
+        public string Error_Number
+        {
+            get { return _error_number; }
+            set { _error_number = value; }
+        }
+
         #endregion
 
         #region variables privadas
@@ -47,6 +71,50 @@ namespace BLL
         #endregion
 
         #region Methods
+
+        public List<Errors> cargar_lista_errores(ref string mensaje_error, ref int numero_error)
+        {
+            connection = cls_DAL.trae_conexion("ServiciosWeb", ref mensaje_error, ref numero_error);
+            if (connection == null)
+            {
+                return null;
+            }
+            else
+            {
+                sql = "exec get_all_errors";
+                ds = cls_DAL.ejecuta_dataset(connection, sql, false, ref mensaje_error, ref numero_error);
+                if (numero_error != 0)
+                {
+                    return null;
+                }
+                else
+                {
+                    return procesarErrores(ds.Tables[0]);
+                }
+            }
+        }
+
+        public void crearError(ref string mensaje_error, ref int numero_error, string Error_ID, string Error_Message, string Time, string Date, string Error_Number)
+        {
+            connection = cls_DAL.trae_conexion("ServiciosWeb", ref mensaje_error, ref numero_error);
+            sql = "exec register_error @Error_ID = '" + Error_ID + "', @Error_Message = '" + Error_Message + "', @Time = '" + Time + "', @Date = '" + Date + "', @Error_Number = '" + Error_Number + "'";
+            //sql = "exec register_user @Cod_user = '" + Cod_User + "', @Username = '" + Username + "', @Pass = '" + Password + "', @Rol = '" + Rol + "', @Email = '" + Email + "', @Question = '" + Question + "', @Answer = '" + Answer + "'";
+            ds = cls_DAL.ejecuta_dataset(connection, sql, false, ref mensaje_error, ref numero_error);
+        }
+
+        private List<Errors> procesarErrores(DataTable dt)
+        {
+            return (from DataRow dr in dt.Rows
+                    select new Errors()
+                    {
+                        //ojete ojete aca
+                        Error_ID = (string)dr["Error_ID"],
+                        Error_Message = dr["Error_Message"].ToString(),
+                        Time = dr["Time"].ToString(),
+                        Date = dr["Date"].ToString(),
+                        Error_Number = dr["Error_Number"].ToString()
+                    }).ToList();
+        }
 
         #endregion
     }
--- ProyectoV/Controllers/ErrorsController.cs
+++ ProyectoV/Controllers/ErrorsController.cs
@@ -5,27 +5,52 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using BLL;
+using ProyectoV.Models;
 using Newtonsoft.Json;
+
+/*
+ 4.6.2. Errores
+Consiste en mostrar una lista con los errores que se han registrado en la aplicación. La 
+consulta tendrá la opción de filtrar por rango de fecha. Se presenta la fecha y hora, numero 
+de error y el mensaje de error correspondiente.
+
+ */
 
 namespace ProyectoV.Controllers
 {
     public class ErrorsController : ApiController
     {
+        string mensaje_error;
+        int numero_error;
+        
         // GET: api/Errors
-        public IEnumerable<string> Get()
+        public List<Errors> Get()
         {
-            return new string[] { "value1", "value2" };
+            List<Errors> listaError = new Errors().cargar_lista_errores(ref mensaje_error, ref numero_error);
+            return listaError;
         }
 
         // GET: api/Errors/5
-        public string Get(int id)
+        public Errors Get(int id) //ID de Error o ID de de error de SQL? Ojete
         {
-            return "value";
+            List<Errors> listaError = new Errors().cargar_lista_errores(ref mensaje_error, ref numero_error);
+            crypting c = new crypting();
+            Errors x = listaError.ElementAt(id); //ojete con la x, es necesaria en errors?
+            Errors spes = new Errors();
+            spes.Error_ID = c.decrypt(x.Error_ID);    //x.Error_ID;
+            spes.Error_Message = c.decrypt(x.Error_Message);
+            spes.Time = c.decrypt(x.Time);
+            spes.Date = c.decrypt(x.Date);
+            spes.Error_Number = c.decrypt(x.Error_Number);
+            return spes;
         }
 
         // POST: api/Errors
-        public void Post([FromBody]string value)
+        public void Post([FromBody]Errors error)
         {
+            Errors create = new Errors();
+            crypting c = new crypting();
+            create.crearError(ref mensaje_error, ref numero_error, c.encrypt(error.Error_ID), c.encrypt(error.Error_Message), c.encrypt(error.Time), c.encrypt(error.Date), c.encrypt(error.Error_Number));
         }
 
         // PUT: api/Errors/5
